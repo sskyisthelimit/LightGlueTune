9035d44ed207aa7bf9ac30e2eb7f33aacad14657
Yalanskyi Roman <keito.eternal@gmail.com>
2025-01-07 21:38:07 +0200

    adjust weight loading logic

--- lightglue/superpoint.py
+++ lightglue/superpoint.py
@@ -141,9 +141,25 @@ class SuperPoint(Extractor):
             c5, self.conf.descriptor_dim, kernel_size=1, stride=1, padding=0
         )
 
-        url = "https://github.com/cvg/LightGlue/releases/download/v0.1_arxiv/superpoint_v1.pth"  # noqa
-        self.load_state_dict(torch.hub.load_state_dict_from_url(url))
+        model = 'SuperPointNet_gauss2'
+        params = {}
+        print("model: ", model)
 
+        def get_model(name):
+            mod = __import__('models.{}'.format(name), fromlist=[''])
+            return getattr(mod, name)
+
+        def modelLoader(model='SuperPointNet', **options):
+            net = get_model(model)
+            net = net(**options)
+            return net
+
+        self.net = modelLoader(model=model, **params)
+
+        checkpoint = torch.load(conf.ckpt_path,
+                                map_location=lambda storage, loc: storage)
+        self.net.load_state_dict(checkpoint['model_state_dict'])
+        
         if self.conf.max_num_keypoints is not None and self.conf.max_num_keypoints <= 0:
             raise ValueError("max_num_keypoints must be positive or None")
 
